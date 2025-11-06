071449ff59f4d4ee06452e78a0903d9764ff8f54
Aniello, Robertino <robertino.aniello@gmail.com>
2025-11-06 22:01:14 +0100

    PolemonBasicInfoServiceTest: not found scenario

--- tests/PokemonApi.Tests/PokemonBasicInfoServiceTest.cs
+++ tests/PokemonApi.Tests/PokemonBasicInfoServiceTest.cs
@@ -9,7 +9,6 @@ namespace PokemonApi.Tests;
 
 public class PokemonBasicInfoServiceTest
 {
-    private readonly IPokemonInfoGateway _pokemonInfoGateway;
     private static readonly PokemonInfo DefaultPokemonInfo = new()
     {
         Name = "Test Pokemon",
@@ -17,22 +16,32 @@ public class PokemonBasicInfoServiceTest
         Habitat = "test",
         IsLegendary = false
     };
+    
+    private readonly IPokemonInfoGateway _pokemonInfoGateway;
+    private readonly PokemonBasicInfoService _sut;
+
     public PokemonBasicInfoServiceTest()
     {
         var mock = new Mock<IPokemonInfoGateway>();
         mock.Setup(m => m.GetAsync(It.IsAny<string>())).Returns(Task.FromResult<PokemonInfo?>(null));
         mock.Setup(m => m.GetAsync(DefaultPokemonInfo.Name)).Returns(Task.FromResult<PokemonInfo?>(DefaultPokemonInfo));
         _pokemonInfoGateway = mock.Object;
+        _sut = new PokemonBasicInfoService(_pokemonInfoGateway);
     }
 
-    
     [Fact]
     public async Task SuccessfulBasicInformation()
     {
-        var sut = new PokemonBasicInfoService(_pokemonInfoGateway);
-        var response = await sut.GetBasicInfoAsync(DefaultPokemonInfo.Name);
+        var response = await _sut.GetBasicInfoAsync(DefaultPokemonInfo.Name);
 
         response.Should().Be(DefaultPokemonInfo);
     }
 
+ [Fact]
+    public async Task NotFound()
+    {
+        var response = await _sut.GetBasicInfoAsync("NotExisting");
+
+        response.Should().BeNull();
+    }
 }
